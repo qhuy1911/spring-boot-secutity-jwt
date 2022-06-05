9a5c382c90aeb740272d290eabeac53e48b9dfe4
qhuy1911 <qhuy1911@gmail.com>
2022-06-05 13:16:25 +0700

    add payloads

--- src/main/java/com/example/springjwt/springbootsecurityjwt/payload/response/MessageResponse.java
+++ src/main/java/com/example/springjwt/springbootsecurityjwt/payload/response/MessageResponse.java
@@ -1,4 +1,17 @@
 package com.example.springjwt.springbootsecurityjwt.payload.response;
 
 public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
